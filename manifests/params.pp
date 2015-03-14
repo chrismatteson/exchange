8952d54baf40332fff13563eeef69bf511f93801
Chris Matteson <chrismatteson@Chriss-MacBook-Pro.local>
2015-03-14 13:14:30 -0700

    updated params

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,13 +2,13 @@ class exchange::params (
   $installerlocation => 'http://care.dlservice.microsoft.com/dl/download/8/1/1/811B7C54-DA45-48AF-A912-0AA2B6FDA02C/Exchange-x64.exe',
   $setupmode,
   $acceptexchangelicense => true,
-  $roles => ['CAS',Mailbox','Hub'],
+  $roles => ClientAccess,Mailbox,
   $installwindowscomponents,
-  $orgname => "$::adsitename Exchange 2013"
+  $orgname,
   $targetdir,
   $sourcedir,
   $updatesdir,
-  $domaincontroller => "$::pdc"
+  $domaincontroller,
   $answerfile,
   $donotstarttransport,
   $enableerrorreporting,
